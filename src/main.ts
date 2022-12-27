3ce3405fe7c16a9d52ee164162b0bbd1301b75c1
Denys Dudnyk <83369962+Denys-Dudnyk@users.noreply.github.com>
2022-12-27 17:14:32 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 	app.setGlobalPrefix('api')
 	app.enableCors()
-	const port = process.env.PORT || 4200
+	const port = process.env.PORT || 3000
 	await app.listen(port)
 }
 bootstrap()
